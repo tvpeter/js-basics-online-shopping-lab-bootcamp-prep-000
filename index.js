c5925d3795ef24fee383b8d3043ad0b47079643a
peter <withtvpeter@gmail.com>
2018-09-30 16:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,10 +68,18 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   
+  for(var i=0; i<cart.length; i++){
+  
   if(cart[i]['itemName'] == item)
   {
-    
+    cart.splice(i, 1);
+    return cart;
+  } 
+  
   }
+  
+  return "That item is not in your cart";
+  
   
 }
 
